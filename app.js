d0a1b9667abf7bc62085712c208611a163e73796
NtetheWonder <44553859+NtetheWonder@users.noreply.github.com>
2022-02-28 23:38:56 +0200

    Few project update

--- app.js
+++ app.js
@@ -4,4 +4,22 @@ $(document).ready(function(){
         $(this).toggleClass('fa-times');
         $('header').toggleClass('toggle');
     })
+
+    $(window).on('scroll', function(){
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+    });
 });
+
+
+function sendEmail(){
+    Email.send({
+       SecureToken : "e18dd471-c4b9-486d-8631-bc8f8ae03dd8",
+        To : "ntethewonder@gmail.com",
+        From : "ntethe990327@gmail.com",
+        Subject : "New Sumit from you website",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}

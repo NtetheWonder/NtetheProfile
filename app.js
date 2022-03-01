17f2a8cf7f74e319d51448cbdc9be3acc8d3e893
NtetheWonder <44553859+NtetheWonder@users.noreply.github.com>
2022-03-01 16:13:54 +0200

    Send update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function sendEmail(){
     Email.send({
        SecureToken : "e18dd471-c4b9-486d-8631-bc8f8ae03dd8",
         To : "ntethewonder@gmail.com",
-        From : "ntethe990327@gmail.com",
+        From : "",
         Subject : "New Sumit from you website",
         Body : "And this is the body"
     }).then(
